86ae1ab0911b13440d6025f39475de905241ab56
TipluMichael <40758606+TipluMichael@users.noreply.github.com>
2018-12-14 16:22:19 +0100

    Auslesen der Datei

--- WpfApp1/Classes/Reader.cs
+++ WpfApp1/Classes/Reader.cs
@@ -11,42 +11,67 @@ namespace WpfApp1
     {
         public void GetData()
         {
+            int count = 0;
+            Dictionary<int, List<string>> strList = new Dictionary<int, List<string>>();
             using (StreamReader sr = new StreamReader(@"...\...\Files\Stundezettel.txt"))
             {
                 while (sr.Peek() >= 0)
                 {
-                    string str;
+                    count++;
+                    List<string> tempRow = new List<string>();
                     string[] strArray;
-                    str = sr.ReadLine();
 
+                    string str = sr.ReadLine();
                     strArray = str.Split('/');
+                    if (!str.StartsWith("#"))
+                    {
+                        for (int i = 0; i < strArray.Length; i++)
+                        {
+                            tempRow.Add(strArray[i]);
+                        }
+                        strList.Add(count, tempRow);
+                    }
 
-                    //DateTime Date = DateTime.ParseExact(strArray[0], "ddMMyyyy", System.Globalization.CultureInfo.InvariantCulture).Date;
-                    //Console.WriteLine(Date);
-                    DateTime Start = DateTime.Parse(strArray[1]);
-                    Console.WriteLine(Start);
-                
-                    //DayData tempDay = new DayData()
-                    //{
-                    //    Date = DateTime.ParseExact(strArray[0], "ddMMyyyy", System.Globalization.CultureInfo.InvariantCulture),
-
-                    //    Start = DateTime.ParseExact(strArray[1], "HHmm",
-                    //                   System.Globalization.CultureInfo.InvariantCulture),
-
-                    //    End = DateTime.ParseExact(strArray[2], "HHmm",
-                    //                   System.Globalization.CultureInfo.InvariantCulture),
-
-                    //    Hours = GetHours(DateTime.ParseExact(strArray[1], "HHmm",
-                    //                  System.Globalization.CultureInfo.InvariantCulture),
-
-                    //                  DateTime.ParseExact(strArray[2], "HHmm",
-                    //                  System.Globalization.CultureInfo.InvariantCulture))
-                    ////};
+                    //DayData tempDay = new DayData(Date, Start, End, Hours, breakTime, reason);
                     //Utility.days_l.Add(tempDay);
                 }
+                ReadDictionary(strList);
             }
 
         }
+
+        private void ReadDictionary(Dictionary<int, List<string>> strList)
+        {
+            foreach (var row in strList)
+            {
+                DayData tempDay = new DayData();
+
+                tempDay.Date = DateTime.ParseExact(row.Value[0], "ddMMyyyy", System.Globalization.CultureInfo.InvariantCulture).Date;
+                tempDay.Start = DateTime.ParseExact(row.Value[1], "HHmm", System.Globalization.CultureInfo.InvariantCulture);
+                tempDay.End = DateTime.ParseExact(row.Value[2], "HHmm", System.Globalization.CultureInfo.InvariantCulture);
+                tempDay.Hours = GetHours(tempDay.Start, tempDay.End);
+                if (row.Value.Count >= 4)
+                {
+                    if (String.IsNullOrEmpty(row.Value[3]))
+                    {
+                        tempDay.BreakTime = 30;
+                    }
+                    else
+                        tempDay.BreakTime = float.Parse(row.Value[3]);
+
+                    if (row.Value.Count >= 5)
+                    {
+                        if (String.IsNullOrEmpty(row.Value[4]))
+                        {
+                            tempDay.Reason = "-";
+                        }
+                        else
+                            tempDay.Reason = row.Value[4];
+                    }
+                }
+            }
+        }
+
         TimeSpan GetHours(DateTime sta, DateTime end)
         {
             TimeSpan diff = sta - end;
